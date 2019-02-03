796e7de34905793a0525ea57446e85b133c269ad
Kobai <vkobayashi106@gmail.com>
2019-02-03 13:57:17 -0500

    Gql and flask works

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from flask_migrate import Migrate, MigrateCommand
 from raven.contrib.flask import Sentry
 from models import *
 
+import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from flask_graphql import GraphQLView
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_POOL_SIZE'] = 10
 app.config['SQLALCHEMY_MAX_OVERFLOW'] = 20
@@ -15,9 +19,46 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/gql?user=kobai&p
 db.init_app(app)
 db.app = app
 
+class BidFileObject(SQLAlchemyObjectType):
+	class Meta:
+		model = BidFile
+		interfaces = (graphene.relay.Node, )
+
+class CreateBid(graphene.Mutation):
+	class Arguments:
+		channel = graphene.String(required=True)
+		stored_at = graphene.String(required=True)
+	bid = graphene.Field(lambda: BidFileObject)
+
+	def mutate(self, info, channel, stored_at):
+		bid = BidFile(channel=channel, stored_at=stored_at)
+		db.session.add(bid)
+		db.session.commit()
+		return CreateBid(bid=bid)
+
+class Query(graphene.ObjectType):
+	node = graphene.relay.Node.Field()
+	all_bids = SQLAlchemyConnectionField(BidFileObject)
+
+class Mutation(graphene.ObjectType):
+	create_bid = CreateBid.Field() 
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
+
+
 @app.route('/')
 def home():
 	return 'hello world'
+
+app.add_url_rule(
+	'/graphql',
+	view_func=GraphQLView.as_view(
+		'graphql',
+		schema=schema,
+		graphiql=True
+	)
+)
 
 manager = Manager(app)
 migrate= Migrate(app,db)
--- models/bid_file.py
+++ models/bid_file.py
@@ -1,13 +1,15 @@
 from models import db
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.schema import Index
+import uuid
 import datetime
 
 class BidFile(db.Model):
 	__tablename__ = 'bid_file'
 	__table_args__ = (Index('bookings_partner_name_partner_id_index', 'channel', 'uploaded_at', unique=True), )
 
-	id = db.Column(UUID(as_uuid=True), unique=True, nullable=False, primary_key=True)
+	# id = db.Column(UUID(as_uuid=True), unique=True, nullable=False, primary_key=True)
+	id = db.Column(UUID(as_uuid=True), unique=True, default=uuid.uuid4(), primary_key=True)
 	channel = db.Column(db.String, index=True)
 	created_at = db.Column(db.DateTime(timezone=True), default=db.func.now())
 	uploaded_at = db.Column(db.DateTime(timezone=True), default=db.func.now())
